f8d3d6365b1212fdc496e877e8118ce0cf9601d5
Ryutera <kuya9823@gmail.com>
2026-02-01 17:54:32 +0000

    contact email changed

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
   return (
   <div className="fixed left-0 right-0 z-20 flex justify-center gap-5 pb-[env(safe-area-inset-bottom)]  text-white opacity-50 sm:bottom-14  bottom-20">
   <Link href="https://github.com/Ryutera"><Github /></Link>
-  <Link href="mailto:teratera0312@icloud.com"><Mail /></Link>
+  <Link href="mailto:kuya9823@gmail.com"><Mail /></Link>
 </div>
   )
 }
